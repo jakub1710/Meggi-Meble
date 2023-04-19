94d9130df8355e071b954ed0d0167ee5b3da8d51
Kuba <jakubkucia93@gmail.com>
2023-04-19 23:17:21 +0200

    Added script open Nav and footeryear

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,25 @@
 const hamburger = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav__mobile')
+const allNavItems = document.querySelectorAll('.nav-item')
+
+const footerYear = document.querySelector('.footer__year');
 
 const showMenu = () => {
     navMobile.classList.toggle('active')
     hamburger.classList.toggle('is-open')
+
+    allNavItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            navMobile.classList.remove('active')
+            hamburger.classList.remove('is-open')
+        })
+    })
 }
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();
 
 hamburger.addEventListener('click', showMenu)
