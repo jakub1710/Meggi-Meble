349d31959c6016ef04c3a72708d2672c497375cb
Kuba <jakubkucia93@gmail.com>
2023-04-25 22:08:34 +0200

    Added function subMenu

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,33 @@
-const hamburger = document.querySelector('.hamburger')
-const navMobile = document.querySelector('.nav__mobile')
-const allNavItems = document.querySelectorAll('.nav-item')
+const hamburger = document.querySelector('.hamburger');
+const navMobile = document.querySelector('.nav__mobile');
+const allNavItems = document.querySelectorAll('.nav-item');
+
+const offerNav = document.querySelector('#offer-nav');
+const subMenu = document.querySelector('.sub-menu');
 
 const footerYear = document.querySelector('.footer__year');
 
+// NAV
 const showMenu = () => {
-    navMobile.classList.toggle('active')
-    hamburger.classList.toggle('is-open')
+	navMobile.classList.toggle('active');
+	hamburger.classList.toggle('is-open');
 
-    allNavItems.forEach((item) => {
-        item.addEventListener('click', () => {
-            navMobile.classList.remove('active')
-            hamburger.classList.remove('is-open')
-        })
-    })
-}
+	allNavItems.forEach((item) => {
+		item.addEventListener('click', () => {
+			// navMobile.classList.remove('active');
+			hamburger.classList.remove('is-open');
+		});
+	});
+};
 
+// FOOTER
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
 handleCurrentYear();
 
-hamburger.addEventListener('click', showMenu)
+hamburger.addEventListener('click', showMenu);
+offerNav.addEventListener('click', () => {
+	subMenu.style.display = 'block';
+});
